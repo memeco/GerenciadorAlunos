81cee866a9c728533c8dd54d1b6d1f2a94ae5cc8
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-28 22:44:37 -0300

    Projeto realizado

---
+++ GerenaciadorAlunos/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace GerenciadorAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Opções do menu
+            const int OPCAO_ADICIONAR_ALUNO = 1;
+            const int OPCAO_BUSCAR_ALUNO = 2;
+            const int OPCAO_EXIBIR_TODOS = 3;
+            const int OPCAO_SAIR = 4;
+
+            // Variável para controlar o loop principal
+            bool sair = false;
+
+            // Loop principal do programa
+            while (!sair)
+            {
+                // Exibir o menu
+                ExibirMenu();
+
+                // Ler a opção do usuário
+                int opcao = int.Parse(Console.ReadLine());
+
+                // Tratar a opção do usuário
+                switch (opcao)
+                {
+                    case OPCAO_ADICIONAR_ALUNO:
+                        AdicionarAluno();
+                        break;
+                    case OPCAO_BUSCAR_ALUNO:
+                        BuscarAluno();
+                        break;
+                    case OPCAO_EXIBIR_TODOS:
+                        ExibirTodosAlunos();
+                        break;
+                    case OPCAO_SAIR:
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+
+        static void ExibirMenu()
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Gerenciador de Alunos");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1 - Adicionar Aluno");
+            Console.WriteLine("2 - Buscar Aluno");
+            Console.WriteLine("3 - Exibir Todos");
+            Console.WriteLine("4 - Sair");
+            Console.WriteLine("-----------------------");
+            Console.Write("Digite a opção desejada: ");
+        }
+
+        static void AdicionarAluno()
+        {
+            Console.Write("Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a turma do aluno: ");
+            string turma = Console.ReadLine();
+
+            // Criar um novo aluno
+            Aluno aluno = new Aluno(nome, turma);
+
+            // Adicionar o aluno ao repositório
+            Repositorio.AdicionaAluno(aluno);
+
+            Console.WriteLine("Aluno adicionado com sucesso!");
+        }
+
+        static void BuscarAluno()
+        {
+            Console.Write("Digite o índice do aluno: ");
+            int indice = int.Parse(Console.ReadLine());
+
+            // Buscar o aluno no repositório
+            Aluno aluno = Repositorio.BuscaAluno(indice);
+
+            if (aluno != null)
+            {
+                Console.WriteLine(aluno);
+            }
+            else
+            {
+                Console.WriteLine("Aluno não encontrado!");
+            }
+        }
+
+        static void ExibirTodosAlunos()
+        {
+            // Obter todos os alunos do repositório
+            Aluno[] alunos = Repositorio.ObterTodosAlunos();
+
+            // Exibir cada aluno
+            foreach (Aluno aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
---
+++ GerenaciadorAlunos/Repositorio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GerenciadorAlunos
+{
+    public static class Repositorio
+    {
+        private static int _index = 0;
+        private static Aluno[] _alunos = new Aluno[100];
+
+        public static void AdicionaAluno(Aluno aluno)
+        {
+            if (_index < _alunos.Length)
+            {
+                _alunos[_index] = aluno;
+                _index++;
+                Console.WriteLine("Aluno adicionado com sucesso!"); // Added confirmation message
+            }
+            else
+            {
+                Console.WriteLine("Limite de alunos atingido!");
+            }
+        }
+
+        public static Aluno BuscaAluno(int indice)
+        {
+            if (indice >= 0 && indice < _index)
+            {
+                return _alunos[indice];
+            }
+
+            return null;
+        }
+
+        public static Aluno[] ObterTodosAlunos()
+        {
+            Aluno[] alunos = new Aluno[_index];
+
+            for (int i = 0; i < _index; i++)
+            {
+                alunos[i] = _alunos[i];
+            }
+
+            return alunos;
+        }
+    }
+}
+s
